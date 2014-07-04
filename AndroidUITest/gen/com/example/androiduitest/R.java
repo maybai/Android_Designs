c546176a7520567b74a2619cff77638e993504af
maybai <may.bai.77@gmail.com>
2014-07-03 21:19:51 -0400

    commit bin files

--- AndroidUITest/gen/com/example/androiduitest/R.java
+++ AndroidUITest/gen/com/example/androiduitest/R.java
@@ -8,6 +8,10 @@
 package com.example.androiduitest;
 
 public final class R {
+    public static final class array {
+        public static final int auto_logout_tie_key=0x7f050000;
+        public static final int auto_logout_time_value=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -18,18 +22,20 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int eg_cute=0x7f020000;
+        public static final int eg_mouse=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button_send=0x7f080003;
-        public static final int container=0x7f080000;
-        public static final int edit_message=0x7f080002;
-        public static final int show_message=0x7f080001;
+        public static final int action_settings=0x7f0a0004;
+        public static final int button_send=0x7f0a0003;
+        public static final int container=0x7f0a0000;
+        public static final int edit_message=0x7f0a0002;
+        public static final int show_message=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -38,16 +44,16 @@ public final class R {
         public static final int fragment_main=0x7f030003;
     }
     public static final class menu {
-        public static final int display_message=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int display_message=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050004;
-        public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050003;
-        public static final int hello_world=0x7f050001;
-        public static final int text_message=0x7f050002;
-        public static final int title_activity_display_message=0x7f050005;
+        public static final int action_settings=0x7f070004;
+        public static final int app_name=0x7f070000;
+        public static final int button_send=0x7f070003;
+        public static final int hello_world=0x7f070001;
+        public static final int text_message=0x7f070002;
+        public static final int title_activity_display_message=0x7f070005;
     }
     public static final class style {
         /** 
@@ -71,10 +77,15 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int checkbox=0x7f040000;
+        public static final int edit_text=0x7f040001;
+        public static final int list=0x7f040002;
     }
 }
